cbeec2a95a411777e1149ce1d6bf874f8aa69779
emiliogarcia1910 <aztroll1000@gmail.com>
2025-03-27 15:34:44 -0300

    Hicimos actividad de variables arreglandolas y le agregamos comentarios

--- hola.php
+++ hola.php
@@ -12,7 +12,16 @@ $numero = "10";  //Está mal puesto, lo detecta como String en vez de entero (ca
 // pais_de_origen= "Uruguay" //Falta cerrar con ; y no hay espacio en el "="
 // $salario = $2000; // El $ está mal puesto, ya que solo debes colocar solamente el numero en la variable
 
+const GRAVEDAD = 9.8; //son espacios en memoria que no cambien en el codigo
 
+const PI= 3.1416;
+const PI= 3.14;
+
+// Error que arrojó: Warning: Constant PI already defined in C:\xampp\htdocs\hola\hola.php on line 18
+//Que no se puede cambiar el valor de una constante
+//En situaciones donde no se deba cambiar los valores y dejarlo fijo para evitar errores por ejemplo
+
+//http://localhost/hola/hola.php
 
 
 
